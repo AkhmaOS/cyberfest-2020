b1ee25e5a8f738febbd6eafebbbaa670bc73689d
Akhma <32985312+AkhmaOS@users.noreply.github.com>
2020-02-25 14:44:24 +0300

    Update settings.py

--- cyberfest_2020/settings.py
+++ cyberfest_2020/settings.py
@@ -24,9 +24,7 @@ SECRET_KEY = 'd+l&962chh=7u)ek4yg4!yy=0+(5(53ul1wwa=)rb7!^*44&(3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    '*'
-]
+ALLOWED_HOSTS = []
 
 # Application definition
 
